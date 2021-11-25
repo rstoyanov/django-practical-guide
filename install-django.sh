492705e919db4b4665b1887674b6697fc4ff6206
rstoyanov <stoyanov.radostin@gmail.com>
2021-11-26 00:38:36 +0200

    the start of the course

---
+++ install-django.sh
@@ -0,0 +1,14 @@
+pip install Django
+
+# call the admin functions
+django-admin
+
+#create a project (Navigate to the app folder first)
+django-admin startproject mypage
+
+# run the web server (Navigate to the app folder first)
+python3 manage.py runserver
+
+# Create a new app/module/feature in the same django page
+python3 manage.py startapp challanges
+
